a24e38dcf94df3e0fa313b39824f8c511ac9884b
Pekka Tukiainen <pekka.tukiainen@philips.com>
2017-12-14 23:30:38 +0200

    Added rudimentary set course functionality.

--- src/app/set-course/set-course.component.ts
+++ src/app/set-course/set-course.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Control } from '../control';
+import { MouseEvent as AGMMouseEvent } from '@agm/core';
+import { LatLng } from '@agm/core/services/google-maps-types';
 
 @Component({
   selector: 'app-set-course',
@@ -8,14 +10,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SetCourseComponent implements OnInit {
 
-  lat = -40.748635;
-  lng = 175.437114;
-  zoom = 6;
+  lat = 60.631216286255004;
+  lng = 24.764760732650757;
+  zoom = 17;
+  totalLength = 0;
+
+  controls: Control[];
 
   constructor() {
+    this.controls = [];
   }
 
   ngOnInit() {
   }
 
+  mapClicked($event: AGMMouseEvent) {
+    this.controls.push({
+      latitude: $event.coords.lat,
+      longitude: $event.coords.lng,
+      height: 0
+    });
+    this.calculateLength();
+  }
+
+  removeControl(control: Control): void {
+    this.controls = this.controls.filter(c => c !== control);
+    this.calculateLength();
+  }
+
+  calculateLength(): void {
+    this.totalLength = 0;
+    let previous: google.maps.LatLng;
+    for (const control of this.controls) {
+      const current: google.maps.LatLng = new google.maps.LatLng(control.latitude, control.longitude);
+      if (previous) {
+        const distance = google.maps.geometry.spherical.computeDistanceBetween(current, previous);
+        this.totalLength += distance;
+      }
+      previous = current;
+    }
+  }
 }
+
